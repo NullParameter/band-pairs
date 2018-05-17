4ca027ece8157e05e67764239ab6f65d8361f523
NullParameter <chris@nullparameter.com>
2018-05-16 19:52:50 -0500

    Brute force approach.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Problem
+
+Given an input file, where each line consists of a comma separated list of band names, 
+find each pair of bands that occurs twice on a single line at least fifty times throughout
+the input file.  Then output those pairs of bands to another file.
+
+# Solution
+
+ 
+
+
+# How to Run
+
+Code can be executed through Gradle from the root directory of the project, supplying both the input and output filepaths, like so:
+`./gradlew run -Pinput="src/main/resources/input.txt" -Poutput="test.csv"`
--- src/main/java/band/pairs/BandPairs.java
+++ src/main/java/band/pairs/BandPairs.java
@@ -1,8 +1,44 @@
 package band.pairs;
 
+import java.io.*;
+import java.util.*;
+
 public class BandPairs {
 
     public static void main(String... args) {
+        final String inputFilePath = args[0];
+        final String outputFilePath = args[1];
 
+        final File inputFile = new File(inputFilePath);
+
+        final Map<String, Integer> bandPairCounts = new HashMap<>();
+
+        try (BufferedReader input = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            while((line = input.readLine()) != null) {
+                List<String> bands = Arrays.asList(line.split(","));
+                Collections.sort(bands);
+
+                for (int i = 0; i < bands.size(); i++) {
+                    for (int j = i + 1; j < bands.size(); j++) {
+                        String bandPair = bands.get(i) + "," + bands.get(j);
+                        bandPairCounts.compute(bandPair, (pair, count) -> count == null ? 1 : count + 1);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.exit(1);
+        } catch (IOException e) {
+            System.exit(2);
+        }
+
+        try (PrintWriter output = new PrintWriter(new FileWriter(outputFilePath))) {
+            bandPairCounts.entrySet().stream()
+                    .filter(e -> e.getValue() >= 50)
+                    .map(Map.Entry::getKey)
+                    .forEach(bands -> output.println(bands));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
